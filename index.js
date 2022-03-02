018c48ecd1015c7a561b7e2f234b7513a01fe07e
LukeTB16 <40920894+LukeTB16@users.noreply.github.com>
2022-03-02 21:10:21 +0100

    v1.2.001

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 const gameScreen = document.getElementById('gameScreen');
 
-document.documentElement.style.setProperty();
+document.documentElement.style.setProperty('--start-width', '200px');
+document.documentElement.style.setProperty('--end-width', '100px');
+document.getElementById('block').style.setProperty('top', '200px');
+// getComputedStyle(document.documentElement).getPropertyValue('--my-variable-name');
+
+var dino = document.getElementById("dino");
+var block = document.getElementById("block");
+
+function jump(){
+    dino.classList.add("animate");
+    if(dino.classList != "animate"){
+        dino.classList.add("animate");
+    }
+    setTimeout(function(){
+        dino.classList.remove("animate");
+    }, 500);
+}
