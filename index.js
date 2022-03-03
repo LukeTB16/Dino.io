629791d416a93019ee5ef45b7ae7eeaa6f540db0
LukeTB16 <40920894+LukeTB16@users.noreply.github.com>
2022-03-03 21:32:49 +0100

    v1.3.0.1

--- index.js
+++ index.js
@@ -15,10 +15,39 @@ function jump(){
     }
     setTimeout(function(){dino.classList.remove("animate");}, 500);
 }
+function getPosition(elem){
+
+    var dims = {offsetLeft:0, offsetTop:0};
+
+    do {
+        dims.offsetLeft += elem.offsetLeft;
+        dims.offsetTop += elem.offsetTop;
+    }
+
+    while (elem = elem.offsetParent);
+
+    return dims;
+}
+
+var checkLose = setInterval(function(){
+    var dino_top = parseInt(window.getComputedStyle(dino).getPropertyValue("top"));
+    var block_left = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
+    if(block_left < 20 && block_left > 0 && dino_top >= 130){
+            block.style.animation = "none";
+            block.style.display = "none";
+            alert("HAI PERSO !");
+    }
+})
 document.documentElement.style.setProperty('--block-start', field_width);
 var fieldPositionInfo = field.getBoundingClientRect();
 var field_height = fieldPositionInfo.height;
 var field_width = fieldPositionInfo.width;
 block.style.setProperty('--block-start', field_width + "px");
 block.style.setProperty('top', field_height-20 + "px");
-console.log(field_width);
+//setInterval(function(){console.log(getPosition(block));}, 50);
+//setInterval(function(){console.log(getPosition(dino));}, 50);
+
+document.addEventListener('long-press', function(e) {
+    e.target.setAttribute('data-editing', 'true');
+    console.log("Sono stato pressato !");
+  });
