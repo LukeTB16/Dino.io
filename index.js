b3a7f9a46ce13c48b60268e2b3e0baa9e0d82b85
LukeTB16 <40920894+LukeTB16@users.noreply.github.com>
2022-03-04 16:56:04 +0100

    v1.3.1

--- index.js
+++ index.js
@@ -1,59 +1,28 @@
-const gameScreen = document.getElementById('gameScreen');
-
-//document.documentElement.style.setProperty('--end-width', '-40px');
-
-// getComputedStyle(document.documentElement).getPropertyValue('--my-variable-name');
-
+/* Define var */
 var dino = document.getElementById("dino");
 var block = document.getElementById("block");
 var field = document.getElementById("field");
-
-function jump(){
-    dino.classList.add("animate");
-    if(dino.classList != "animate"){
-        dino.classList.add("animate");
-    }
-    setTimeout(function(){dino.classList.remove("animate");}, 500);
-}
-function getPosition(elem){
-
-    var dims = {offsetLeft:0, offsetTop:0};
-
-    do {
-        dims.offsetLeft += elem.offsetLeft;
-        dims.offsetTop += elem.offsetTop;
-    }
-
-    while (elem = elem.offsetParent);
-
-    return dims;
-}
-var checkLose = setInterval(function(){
-    var dino_top = parseInt(window.getComputedStyle(dino).getPropertyValue("top"));
-    var block_left = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
-    if(block_left < 20 && block_left > 0 && dino_top >= 130){
-            block.style.animation = "none";
-            block.style.display = "none";
-            alert("HAI PERSO !");
-    }
-});
-document.documentElement.style.setProperty('--block-start', field_width);
 var fieldPositionInfo = field.getBoundingClientRect();
 var field_height = fieldPositionInfo.height;
 var field_width = fieldPositionInfo.width;
-block.style.setProperty('--block-start', field_width + "px");
-block.style.setProperty('top', field_height-20 + "px");
-//setInterval(function(){console.log(getPosition(block));}, 50);
-//setInterval(function(){console.log(getPosition(dino));}, 50);
-document.documentElement.style.setProperty('--dino-jump1', 100 + "px");
-document.documentElement.style.setProperty('--dino-jump2', 100 + "px");
-document.documentElement.style.setProperty('--dino-jump3', 100 + "px");
 
-document.addEventListener('long-press', jump_high);
-function jump_high() {
-    document.documentElement.style.setProperty('--dino-jump1', 80 + "px");
-    document.documentElement.style.setProperty('--dino-jump2', 70 + "px");
-    document.documentElement.style.setProperty('--dino-jump3', 80 + "px");
-    console.log("PRESSING!");
-    document.removeEventListener('long-press', jump_high); /* NON FUNZIONA !*/
+
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
+var time;
+function keyDownHandler(e) {
+  if (e.keyCode == 38){
+    time = Date.now();
+    console.log("Pressed");
+  }
 }
+function keyUpHandler(e) {
+  if (e.keyCode == 38){
+    console.log("Released");
+    let totale = Date.now() - time; // totale > 300 ==> long jump
+    console.log(totale);
+  }
+}
+    
+
+
