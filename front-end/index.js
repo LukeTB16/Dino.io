59ca520cd8df8a334590ea0f46b1ded9b6bade07
LukeTB16 <40920894+LukeTB16@users.noreply.github.com>
2022-04-23 20:18:56 +0200

    added new enemies

--- front-end/index.js
+++ front-end/index.js
@@ -14,15 +14,8 @@ var canvas = document.getElementById("canvas");
 var design = canvas.getContext("2d"); // return 2d drawing context on the field
 // IMAGES
 // Dino image
-var dinoReady = false;
 const dinoImage = new Image();
-dinoImage.onload = function () {
-  dinoReady = true;
-};
 dinoImage.src = "graphics/dino_sprites_new.png";
-// Obstacle images
-const obstacle1Image = new Image();
-obstacle1Image.src = "graphics/obstacles/obstacle.png";
 // Ground image
 const groundImage = new Image();
 groundImage.src = "graphics/ground.png";
@@ -38,27 +31,37 @@ score_board.src = "graphics/score_board.png";
 // Rope image
 const rope = new Image();
 rope.src = "graphics/rope.png";
+// Enemy image
+const ob1 = new Image();
+ob1.src = "graphics/obstacles/obstacle1.png";
+const ob2 = new Image();
+ob2.src = "graphics/obstacles/obstacle2.png";
+const bird = new Image();
+bird.src = "graphics/obstacles/bird.png";
 
 // Needed declarations
 var time;
 var collision = true;
-let obstXPos; // for collision only
-let obstYPos; // for collision only
-let dinoXPos;
-let dinoYPos;
-let frameX = 0; // coordinates to take frames of sprite
-let frameY = 4; // must be 0 bc we have sprites in same line in this case
+let frameX = 0; // X coordinate of dino sprite
+let frameY = 2; // Y coordinate of dino sprite
+let obframeX = 0; // X coordinate of obstacle sprite
+let obframeY = 0; // Y coordinate of obstacle sprite
+let birdframeX = 0; // X coordinate of bird sprite
+let birdframeY = 0; // Y coordinate of bird sprite
 let gameFrame = 0;
-const shakeFrame = 8;
+let obgameFrame = 0;
+let birdgameFrame = 0;
+let shakeFrame = 12; // frame number of shake
+let obshakeFrame = 12; // frame number of shake
+let birdshakeFrame = 12; // frame number of bird
 let jump_counter = 0;
 let gameOver = false;
-var distance;
-var sumOfRadii;
-var enemy_online_counter = 0;
-var enemy_alive_status = false;
 var jump_count = 0;
+var cover_count = 0;
+var delay = 1000;
 var id = null;
 var gameStart = false;
+var pos;
 let ws = new WebSocket("ws://localhost:8080"); // open parallel client channel using sockets
 var form = document.getElementById("form");
 var single = document.getElementById("single");
@@ -69,36 +72,45 @@ var partyCode = document.getElementById("partyCode");
 var players = document.getElementById("players");
 var lobby = document.getElementById("lobby");
 */
+// TODO: change lose screen with Photoshop
 // Define keyboard keys
 const keyboard_keys = (function () {
   document.addEventListener("keydown", keyDownHandler, false);
   document.addEventListener("keyup", keyUpHandler, false);
   const keyboard_keys = {
-    // up is when the button is not pressed, down is pressed
-    down: false,
-    up: false,
+    down: false,  // 'down' is when arrow up button is pressed
+    up: false,  // 'up' when arrow up button is released
+    cover: false  // 'cover' when arrow down button is pressed 
   };
   function keyDownHandler(e) {
-    if (e.keyCode == 38) { // arrow up
+    if (e.keyCode == 38) { // arrow up pressed
       keyboard_keys.down = true;
       keyboard_keys.up = false;
+      frameY = 0;
       if (jump_count >= 10) {
         keyboard_keys.down = false;
         keyboard_keys.up = true;
       }
       jump_count = jump_count + 1;
     }
-    if (e.keyCode == 40) {// arrow down
-      keyboard_keys.down = true;
-      keyboard_keys.up = false;
+    if (e.keyCode == 40) { // arrow down pressed
+      frameX = 7;
+      frameY = 1;
+      keyboard_keys.cover = true;
     }
 
   }
   function keyUpHandler(e) {
-    if (e.keyCode == 38) {
+    if (e.keyCode == 38) { // arrow up pressed released
       keyboard_keys.up = true;
       keyboard_keys.down = false;
       jump_count = 0;
+      frameY = 2;
+    }
+    if (e.keyCode == 40) { // arrow down released
+      frameY = 2;
+      frameX = 0;
+      keyboard_keys.cover = false;
     }
   }
   return keyboard_keys;
@@ -106,37 +118,36 @@ const keyboard_keys = (function () {
 
 // Define dino
 const dino = {
-  x: 0,
+  x: 100,
   y: 0,
-  dx: 0,
   dy: 0,
-  frameWidth: 700, // prec. 519, 8160 x 2360
-  frameHeight: 475, // prec. 413
-  frameCount: 6,
+  frameWidth: 680, // prec. 519, 8160 x 2360
+  frameHeight: 472, // prec. 413
+  frameCount: 8,  // number of sprite in a row
   jumpPower: -18,
   gravity: 0.899,
   drag: 0.9,
   score: 0,
   lifes: 2,
   draw(design) {
-    if (dinoReady) {
-      dinoXPos = 100;
-      dinoYPos = this.dy + 500;
-      design.drawImage(
-        dinoImage,
-        this.frameWidth * frameX,
-        this.frameHeight * frameY,
-        400,
-        475,
-        dinoXPos, // X position of dino
-        dinoYPos, // Y position of dino
-        250, // width of dino
-        275 // height of dino
-      );
-    }
+    this.y = this.dy + 540;
+    design.drawImage(
+      dinoImage,
+      this.frameWidth * frameX, // X position of dino in the png
+      this.frameHeight * frameY, // Y position of dino in the png
+      this.frameWidth, // width of dino in the png
+      this.frameHeight, // height of dino in the png
+      this.x, // X position of dino
+      this.y, // Y position of dino
+      350, // width of dino
+      322 // height of dino
+    );
     // Frame management
     if (Math.floor(gameFrame % shakeFrame) == 0) {
-      if (frameX < this.frameCount - 1) {
+      if (frameX == 7 && frameY == 1) {  // freeze dino down position
+        gameFrame--;
+      }
+      else if (frameX < this.frameCount - 1) {
         frameX++;
       } else {
         frameX = 0;
@@ -153,52 +164,8 @@ const dino = {
     // gravity and ground contact
     this.dy = this.dy + this.gravity;
     this.dy = this.dy * this.drag;
-    if (dinoXPos + (this.frameHeight - 150) + this.dy <= design.canvas.height) {
+    if (this.x + (this.frameHeight - 150) + this.dy <= design.canvas.height) {
       this.dy = this.dy + this.gravity;
-    }
-  },
-  checkCollision() {
-    // rect1.x < rect2.x + rect2.width && rect1.x + rect1.width > rect2.x &&
-    // rect1.y < rect2.y + rect2.height && rect1.y + rect1.height > rect2.y
-    /* RECTANGULAR COLLISION
-    if(dinoXPos < obstXPos + obstacle.width &&
-      dinoXPos + this.width > obstXPos &&
-      dinoYPos < obstYPos + obstacle.height &&
-      dinoYPos  + this.height > obstYPos){  // collision detection
-        lifes = lifes - 1;
-        gameOver = true;
-    }
-    */
-
-    // CIRCLE COLLISION
-    const circle1 = {
-      x: dinoXPos + 250,
-      y: dinoYPos + 200,
-      radius: 130,
-    };
-    const circle2 = {
-      x: obstXPos-25,
-      y: obstYPos+95,
-      radius: 60,
-    };
-    design.beginPath(); // DRAW - START PRINT CIRCLES
-    design.arc(circle1.x, circle1.y, circle1.radius, 50, 0, Math.PI * 2);
-    design.arc(circle2.x, circle2.y, circle2.radius, 150, 0, Math.PI * 2);
-    design.stroke(); //DRAW - END PRINT CIRCLES
-    let dx = circle2.x - circle1.x;
-    let dy = circle2.y - circle1.y;
-    let distance = Math.sqrt(dx * dx + dy * dy);
-    let sumOfRadii = circle1.radius + circle2.radius;
-    if (distance < sumOfRadii && collision == true && gameOver == false) {
-      this.lifes = this.lifes - 1;
-      collision = false;
-      design.fillStyle = "red";
-      design.fillRect(0, 0, design.canvas.width, design.canvas.height);
-    } else if (distance > sumOfRadii && collision == false) {
-      collision = true;
-    }
-    if (this.lifes == 0) {
-      gameOver = true;
     }
   },
 };
@@ -206,10 +173,9 @@ const dino = {
 const ground = {
   x: 0,
   y: 700,
-  dx: 0,
   height: 400,
   width: window.innerWidth,
-  speed: 3,
+  speed: 2,
   draw() {
     design.drawImage(groundImage, this.x, this.y, this.width, this.height);
     design.drawImage(
@@ -252,30 +218,72 @@ const background = {
     );
   },
 };
-// Define obstacle enemy
+// Define obstacle
 const obstacle = {
-  x: 0,
-  y: 0,
+  obstXPos: 0,
+  obstYPos: 0,
   dx: 0,
-  dy: 0, // move distance
-  width: 80,
-  height: 60,
-  onGround: false,
-  speed: 3,
-  draw(design) {
-    obstXPos = ground.x + window.innerWidth;
-    obstYPos = ground.y;
-    // void ctx.drawImage(image, sx, sy, sWidth, sHeight, dx, dy, dWidth, dHeight);
-    design.drawImage(
-      obstacle1Image,
-      obstXPos-100,
-      obstYPos+40,
-      this.width+40,
-      this.height+40
-    );
+  dy: 700,
+  frameWidth: 299,
+  frameHeight: 281,
+  frameCount: 43,
+  speed: 1,
+  draw(image, delay, pos, type) {
+    this.obstXPos = this.dx + delay + window.innerWidth;
+    this.obstYPos = this.dy + pos;
+    // syntax ctx.drawImage(image, sx, sy, sWidth, sHeight, dx, dy, dWidth, dHeight);
+    if (type == 0) {  // 0 => bird
+      design.drawImage(
+        bird,
+        598 * birdframeX,
+        402 * birdframeY,
+        598,
+        402,
+        this.obstXPos,
+        this.obstYPos,
+        200,
+        150
+      );
+      // Frame management
+      if (Math.floor(birdgameFrame % birdshakeFrame) == 0) {
+        if (birdframeX < 3) {  // 4 frame
+          birdframeX++;
+        } else {
+          birdframeX = 0;
+        }
+      }
+      birdgameFrame++;
+    }
+    else if (type == 1) { // 1 => others obstacles
+      design.drawImage(
+        image,
+        this.frameWidth * obframeX,
+        this.frameHeight * obframeY,
+        this.frameWidth,
+        this.frameHeight,
+        this.obstXPos,
+        this.obstYPos,
+        150,
+        150
+      );
+      // Frame management
+      if (Math.floor(obgameFrame % obshakeFrame) == 0) {
+        if (obframeX < this.frameCount - 1) {
+          obframeX++;
+        } else {
+          obframeX = 0;
+        }
+      }
+      obgameFrame++;
+    }
+    this.dx = this.dx - this.speed; // comment this line to stop game for debugging
+    if (this.obstXPos < 0 - (window.innerWidth)) {
+      this.dx = 0;
+      this.obstXPos = this.dx + window.innerWidth;
+      update_delay();
+    }
   },
 };
-
 // Define leaderboard
 const leaderboard = {
   x: window.innerWidth - 600,
@@ -311,11 +319,15 @@ const status_board = {
   width: 700,
   height: 200,
   draw(context) {
-    design.drawImage(rope, this.x, this.y - 55, this.width, this.height + 1000);
+    design.drawImage(rope,
+      this.x,
+      this.y - 120,
+      this.width,
+      this.height + 1000);
     design.drawImage(
       rope,
       this.x + 265,
-      this.y - 55,
+      this.y - 120,
       this.width,
       this.height + 1000
     );
@@ -333,32 +345,69 @@ const status_board = {
       this.width + 700,
       this.height + 380
     );
-    design.drawImage(
-      score_board,
-      this.x,
-      this.y + 200,
-      this.width + 700,
-      this.height + 380
-    );
     context.fillStyle = "#ffc61a"; // #66ff66
     context.font = "42px Secular One";
     context.fillText("Vite rimaste: " + dino.lifes, this.x + 25, this.y + 55); // Score board
     context.fillText("Score: " + dino.score, this.x + 80, this.y + 155); // Score board
-    context.fillText("Status: Offline", this.x + 27, this.y + 255); // Score board
   },
-  update() {},
+  update() { },
 };
 
 
 // Functions
+function getRndInteger(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+function update_delay() {
+  delay = getRndInteger(0, 2000);
+}
+function checkCollision(d1, ob) { // 2: 100/70 3: 160/270 4: 370/270
+  // CIRCLE COLLISION
+  let circle1 = {
+    x: d1.x + 120,
+    y: d1.y + 170,
+    radius: 120,
+  };
+  let circle2 = {
+    x: ob[0].obstXPos + ob[1],
+    y: ob[0].obstYPos + ob[2],
+    radius: 90,
+  };
+  if (keyboard_keys.cover == true) {
+    circle1.y = circle1.y + 100;
+  }
+  //design.beginPath(); // DRAW - START PRINT CIRCLES
+  design.arc(circle1.x, circle1.y, circle1.radius, 50, 0, Math.PI * 2);
+  design.arc(circle2.x, circle2.y, circle2.radius, 150, 0, Math.PI * 2);
+  //design.stroke(); // DRAW - END PRINT CIRCLES
+  let dx = circle2.x - circle1.x;
+  let dy = circle2.y - circle1.y;
+  let distance = Math.sqrt(dx * dx + dy * dy);
+  let sumOfRadii = circle1.radius + circle2.radius;
+  if (distance < sumOfRadii && collision == true && gameOver == false) {
+    d1.lifes = d1.lifes - 1;
+    collision = false;
+    design.fillStyle = "red";
+    design.fillRect(0, 0, design.canvas.width, design.canvas.height);
+  } else if (distance > sumOfRadii && collision == false) {
+    collision = true;
+  }
+  if (d1.lifes == 0) {
+    gameOver = true;
+  }
+}
 function score_update(player) {
   setInterval(() => {
     player.score = player.score + 1;
   }, 1000);
 }
 function speed_update(object) {
-  setInterval(() => {
-    object.speed = object.speed + 1;
+  var s_update = setInterval(() => {
+    obstacle.speed = obstacle.speed + 0.25;
+    object.speed = object.speed + 0.25;
+    if (object.speed >= 10) {
+      clearInterval(s_update);
+    }
   }, 5000);
 }
 function draw_screen() {
@@ -371,7 +420,7 @@ function get_lead() {
   }
   ws.send(JSON.stringify(payload));
 }
-function send_lead(nick, s){
+function send_lead(nick, s) {
   const payload = {
     "method": "update_lead",
     "nickname": nick,
@@ -381,16 +430,27 @@ function send_lead(nick, s){
 }
 function main() {
   if (!gameOver && gameStart) {
+    let o1 = obstacle;
+    let o2 = obstacle;
+    let o3 = obstacle;
+    let d = dino;
+    let comp1 = [o1, 100, 70];
+    let comp2 = [o2, 160, 270];
+    let comp3 = [o3, 370, 270];
     design.clearRect(0, 0, canvas.width, canvas.height);
     draw_screen();
     background.draw();
     leaderboard.draw(design);
     ground.draw();
     ground.update();
-    obstacle.draw(design);
-    dino.checkCollision();
-    dino.game();
-    dino.draw(design);
+    o1.draw(ob1, -500, 0, 1);
+    o2.draw(ob2, -700, 0, 1);
+    o3.draw(bird, -800, -200, 0);
+    checkCollision(d, comp1);
+    checkCollision(d, comp2);
+    checkCollision(d, comp3);
+    d.game();
+    d.draw(design);
     status_board.draw(design);
     id = requestAnimationFrame(main);
   } else if (gameOver) {
@@ -400,7 +460,7 @@ function main() {
 }
 // switch screen
 function change_screen() {
-  if(nickname.value != ""){
+  if (nickname.value != "") {
     gameStart = true;
     document.body.style.backgroundImage = "url('graphics/back_blurred_base.png')";
     single.remove();
@@ -410,13 +470,13 @@ function change_screen() {
     get_lead();
     main();
   }
-  else{
+  else {
     window.alert("Insert nickname first!");
     id = cancelAnimationFrame(main);
   }
   console.log("Nickname: ", nickname.value); // nickname given from the user
 }
-if(gameStart){
+if (gameStart) {
   score_update(dino);
   speed_update(ground);
 }
@@ -481,16 +541,16 @@ document.body.appendChild(status_text);
 */
 
 single.addEventListener("click", e => {
-  if(nickname.value == ""){
+  if (nickname.value == "") {
     window.alert("Insert nickname first!");
   }
-  else{
+  else {
     const payload = {
-        "method": "create",
-        "clientId": clientId,
-        "nickname": nickname.value
-      }
-      ws.send(JSON.stringify(payload));
+      "method": "create",
+      "clientId": clientId,
+      "nickname": nickname.value
+    }
+    ws.send(JSON.stringify(payload));
   }
 });
 // managing requests client side
@@ -502,7 +562,7 @@ ws.onmessage = (message) => {
     clientId = response.clientId;
     console.log("Client successfully set, ID: " + clientId);
   }
-  
+
   // create
   if (response.method === "create") {
     clientId = response.clientId;
